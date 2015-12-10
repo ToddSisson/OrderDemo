563df26e1396879e23a3136fb2efee9b4ce129dd
Todd Sisson <todd.a.sisson@gmail.com>
2015-12-10 09:33:41 -0600

    Added initial files

---
+++ Order.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderDemo
+{
+    class Order
+    {
+        private const double PRICE =  19.95;
+        public int OrderNumber { get; set; }
+        public string CustomerName { get; set; }
+        public int QuantityOrdered { get; set; }
+        public double TotalPrice
+        {
+            get
+            {
+                return QuantityOrdered* PRICE;
+            }
+        }
+
+        public Order(int orderNumber, string customerName, int quantityOrdered)
+        {
+            OrderNumber = orderNumber;
+            CustomerName = customerName;
+            QuantityOrdered = quantityOrdered;
+        }
+
+        public override bool Equals(object obj)
+        {
+            bool isEqual;
+            Order temp = (Order)obj;
+            if (OrderNumber == temp.OrderNumber)
+                isEqual = true;
+            else
+                isEqual = false;
+            return isEqual; 
+        }
+
+        public override int GetHashCode()
+        {
+            return OrderNumber;
+        }
+
+        public override string ToString()
+        {
+            return ($"\nOrder Number: {OrderNumber}\nCustomer Name: {CustomerName}\nQuanity Ordered: {QuantityOrdered}\nPrice per Object: {PRICE}\nTotal Price: {TotalPrice}");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order[] orders = new Order[3];
+
+            for (int i = 0; i < orders.Length; i++)
+            {
+                Console.Write("Enter an order ID: ");
+                int orderID = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter a name: ");
+                string customerName = Console.ReadLine();
+                Console.Write("Enter a quantity ordered: ");
+                int quantityOrdered = Convert.ToInt32(Console.ReadLine());
+                
+                orders[i] = new Order(orderID, customerName, quantityOrdered);
+
+            }
+
+            for (int i = 0; i < orders.Length; i++)
+            {
+                for (int x = 0; x < orders.Length; x++)
+                {
+                   while (orders[x].Equals(orders[i]) && orders[x] != orders[i])
+                    {
+                        Console.WriteLine("Order numbers must be unique. Enter a new one");
+                        orders[i].OrderNumber = Convert.ToInt32(Console.ReadLine());
+                    } 
+                }
+
+                Console.WriteLine(orders[i].ToString());
+            }
+        }
+    }
+}
